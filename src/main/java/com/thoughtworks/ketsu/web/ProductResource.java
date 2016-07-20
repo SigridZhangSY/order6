29e07861294eef1cb9cc17406ba9364306da19ad
SigridZhangSY <sigridzhang0603@163.com>
2016-07-20 20:06:58 +0800

    return 200 when get all orders

--- src/main/java/com/thoughtworks/ketsu/web/ProductResource.java
+++ src/main/java/com/thoughtworks/ketsu/web/ProductResource.java
@@ -5,9 +5,7 @@ import com.thoughtworks.ketsu.infrastructure.records.ProductRecord;
 import com.thoughtworks.ketsu.web.exception.InvalidParameterException;
 import com.thoughtworks.ketsu.web.jersey.Routes;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -33,5 +31,11 @@ public class ProductResource {
         if(fields.size() > 0)
             throw new InvalidParameterException(fields);
         return Response.created(routes.productUri(productRepository.createProduct(info))).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String listProducts(){
+        return "OK";
     }
 }
