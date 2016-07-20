ee2bf16ef0da4944d6c0335a3111992c688fa9f5
SigridZhangSY <sigridzhang0603@163.com>
2016-07-20 17:44:35 +0800

    return 201 when create product with specified parameter

--- src/main/java/com/thoughtworks/ketsu/web/ProductResource.java
+++ src/main/java/com/thoughtworks/ketsu/web/ProductResource.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.ketsu.web;
 
+import com.thoughtworks.ketsu.infrastructure.core.ProductRepository;
 import com.thoughtworks.ketsu.infrastructure.records.ProductRecord;
 import com.thoughtworks.ketsu.web.jersey.Routes;
 
@@ -9,13 +10,16 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Map;
 
 @Path("products")
 public class ProductResource {
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response createProduct(@Context Routes routes){
-        return Response.created(routes.productUri(new ProductRecord(1))).build();
+    public Response createProduct(Map<String, Object> info,
+                                  @Context Routes routes,
+                                  @Context ProductRepository productRepository){
+        return Response.created(routes.productUri(productRepository.createProduct(info))).build();
     }
 }
--- src/test/java/com/thoughtworks/ketsu/resources/ProductResourceTest.java
+++ src/test/java/com/thoughtworks/ketsu/resources/ProductResourceTest.java
@@ -12,7 +12,6 @@ import javax.ws.rs.core.Response;
 
 import java.util.regex.Pattern;
 
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -25,7 +24,7 @@ public class ProductResourceTest extends ApiSupport {
     }
 
     @Test
-    public void should_return_201_when_create_product(){
+    public void should_return_201_when_create_product_with_specified_parameter(){
         Response post = post("products", TestHelper.productMap("apple"));
         assertThat(post.getStatus(), is(HttpStatus.CREATED_201.getStatusCode()));
         assertThat(Pattern.matches(".*?/products/[0-9-]*", post.getLocation().toASCIIString()), is(true));
